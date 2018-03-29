80d7c253e854f374cf72dbdf3aefa7407b48268f
madhanrajR <madhanit31@gmail.com>
2018-03-29 11:10:23 +0530

    Update Financial.sol

--- contracts/Financial.sol
+++ contracts/Financial.sol
@@ -1,14 +1,11 @@
 pragma solidity^0.4.0;
-<<<<<<< HEAD
-import "./borrower.sol";
-=======
 import "./Borrower1.sol";
->>>>>>> 92386bfb62f436a5f9b1dba7c52e156b0e14c960
-contract fi is bor{
+contract financial is borrower{
     uint256 loanamount;
     uint256 tokenvalue;
     uint256 eth;
     uint256 ethervalue;
+    uint256 etheramount;
     struct loandetails
     {
         address to;
@@ -16,34 +13,37 @@ contract fi is bor{
         uint256 value;
        uint256 duration;
     }
-    mapping(address=> loandetails)detail;
-    function ethertransfer(address to,uint256 tokenvalue)public view returns(uint256,uint256)
+    mapping(address=> loandetails)public detail;
+    function get(address fi)public constant returns(address,uint256)
+    {
+        
+        return(fi,msg.value);
+    }
+    function ethertransfer(address borrower,uint256 tokenvalue)public view returns(uint256,uint256)
     {
         eth = tokenvalue * 1 ether;
         loanamount = eth/100 * 80;
          ethervalue = loanamount / 1 ether;
-        detail[to].to = to;
-        detail[to].value = tokenvalue;
-        detail[to].loanamount = ethervalue;
-         detail[to].duration = 12;
-        return(detail[to].duration,detail[to].loanamount);
+         
+        detail[borrower].to = borrower;
+        detail[borrower].value = tokenvalue;
+        detail[borrower].loanamount = ethervalue;
+         detail[borrower].duration = 12;
+        return(detail[borrower].duration,detail[borrower].loanamount);
     }
      
     function sendether(address to) public payable 
     {
         to.transfer(msg.value);
+        
     }
     
-    function viewether(address to)public constant returns(uint256,uint256)
+    function viewether(address from)public constant returns(uint256)
     {
-        return(detail[to].duration,detail[to].loanamount);
+        return(etheramount);
     }
     
-    function sellloan(address to,uint256 value)
-    {
-        balance[msg.sender] -=value;
-        balance[to] +=value;
-    }
+   
 }
 
 
