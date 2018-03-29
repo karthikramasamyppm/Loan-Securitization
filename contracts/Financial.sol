49f65149575f24b7aa73d95523e99335fac697f6
madhanrajR <madhanit31@gmail.com>
2018-03-29 16:41:33 +0530

    Update Financial.sol

--- contracts/Financial.sol
+++ contracts/Financial.sol
@@ -1,49 +1,58 @@
 pragma solidity^0.4.0;
 import "./Borrower1.sol";
 contract financial is borrower{
-    uint256 loanamount;
-    uint256 tokenvalue;
-    uint256 eth;
-    uint256 ethervalue;
-    uint256 etheramount;
+    
+    uint256 FI_interest;
+    uint256 FI_duration;
+    uint256 initial_ether;
+    uint256 loan_count;
+    address financial_institution = 0x3eF74E84B495FC03E41adCB516E1D109D4e1F991;
     struct loandetails
     {
-        address to;
+        address borrower;
         uint256 loanamount;
-        uint256 value;
-       uint256 duration;
+        uint256 token;
+        uint256 duration;
     }
     mapping(address=> loandetails)public detail;
-    function get(address fi)public constant returns(address,uint256)
+    function register(uint256 initial_value,uint256 interest,uint256 duration)public
     {
-        
-        return(fi,msg.value);
+        initial_ether = initial_value * 1 ether;
+        FI_interest=interest;
+        FI_duration=duration;
     }
-    function ethertransfer(address borrower,uint256 tokenvalue)public view returns(uint256,uint256)
+    function existing()public view returns(address,uint256,uint256,uint256)
     {
-        eth = tokenvalue * 1 ether;
-        loanamount = eth/100 * 80;
-         ethervalue = loanamount / 1 ether;
-         
-        detail[borrower].to = borrower;
-        detail[borrower].value = tokenvalue;
-        detail[borrower].loanamount = ethervalue;
-         detail[borrower].duration = 12;
-        return(detail[borrower].duration,detail[borrower].loanamount);
+         return(financial_institution,initial_ether,FI_interest,FI_duration);
+    }
+    
+    
+    function loancalculation(address borrower,uint256 tokenvalue)public 
+    {
+        detail[borrower].borrower = borrower;
+        detail[borrower].token = tokenvalue;
+        detail[borrower].loanamount = (((tokenvalue * 1 ether)/100 * 80) / 1 ether);
+        detail[borrower].duration = 12;
+        loan_count +=1;
+        //return(detail[borrower].duration,detail[borrower].loanamount);
     }
      
-    function sendether(address to) public payable 
+    function sendether(address borrower) public payable 
     {
-        to.transfer(msg.value);
-        
+        borrower.transfer(msg.value);
+        initial_ether -= detail[borrower].loanamount;
     }
     
-    function viewether(address from)public constant returns(uint256)
+    function viewether(address borrower)public view returns(uint256)
     {
-        return(etheramount);
+        return(detail[borrower].loanamount);
     }
     
-   
+   /* function sellloan(address to,uint256 value)
+    {
+        balance[msg.sender] -=value;
+        balance[to] +=value;
+    }*/
 }
 
 
