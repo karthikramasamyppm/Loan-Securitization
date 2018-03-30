9e9307085b2052f96a305600d6bd7aca8b34e169
sangi50 <37014050+sangi50@users.noreply.github.com>
2018-03-30 16:52:08 +0530

    Update borrower.sol

--- contracts/borrower.sol
+++ contracts/borrower.sol
@@ -1,16 +1,15 @@
 pragma solidity ^0.4.0;
 import "./ERC20.sol";
-contract bor is ERC20{
+contract borrower is ERC20{
  
  string public name; 
  string public symbol; 
  uint256 public totalSupply; 
- uint256 value;
- 
- address public owner; 
+
+ address public borrower_address; 
  mapping (address => uint256)  balance; 
- function bor(){
-     owner=msg.sender;
+ function borrower() public{
+     borrower_address=msg.sender;
      totalSupply = 10;
      balance[msg.sender] = totalSupply;
      name = "aa"; 
@@ -18,26 +17,18 @@ contract bor is ERC20{
       
      } 
     
-     function totalSupply() constant returns (uint256)
+     function totalSupply() public constant returns (uint256)
     {
         return totalSupply;
     }
     
-    
-
-function balanceOf(address _addr)public constant returns(uint256) {
-   return balance[_addr];
-}
-function transferfrom(address from,address to,uint256 value)
-{
-    balance[from] -= value;
-    balance[to] +=value;
-   
-}
-
-
-
-
+  
+    function transferfrom(address from,address to,uint256 value)internal
+    {
+        balance[from] -= value;
+       balance[to] +=value;
+      
+    ]
 
 
 }
