b4b46640f394ca07d56b64f13788e12322582bf0
Santhoshpriya097 <37013427+Santhoshpriya097@users.noreply.github.com>
2018-03-31 17:06:41 +0530

    Update Financial1.sol

--- contracts/Financial1.sol
+++ contracts/Financial1.sol
@@ -18,17 +18,9 @@ contract financial  {
       uint256 balance_amount_investor;
      mapping(address => uint256)balance;
     function financial () public {
-=======
-import "./borrower.sol";
-contract finance is borrower {
-    
-    uint256 FI_interest;
-    uint256 FI_duration;
-    uint256 initial_ether;
-    uint256 loan_count;
-    address financial_institution;
+
     function finance () public{
->>>>>>> e33bb485124b96eebe86669e641f20f5e4e0e2ef
+
         financial_institution =msg.sender;
     }
     struct loandetails
@@ -37,7 +29,7 @@ contract finance is borrower {
         uint256 loanamount;
         uint256 token;
         uint256 duration;
-<<<<<<< HEAD
+
         uint256 tot_amount;
         
     }
@@ -147,49 +139,3 @@ contract finance is borrower {
   
 
 
-=======
-        
-    }
-    mapping(address=> loandetails)public detail;
-  //  mapping(uint256 => uint256)public balanceOf;
-    
-    function register(uint256 interest,uint256 duration)public payable
-    {
-        initial_ether = msg.value / 1 ether ;
-        FI_interest=interest;
-        FI_duration=duration;
-    }
-    function existing()public view returns(address,uint256,uint256,uint256)
-    {
-         return(financial_institution,initial_ether,FI_interest,FI_duration);
-    }
-    
-    
-    function borrowMoney(address borrower,uint256 tokenvalue)public 
-    {
-        transferfrom(borrower,financial_institution,tokenvalue);
-        detail[borrower].borrower = borrower;
-        detail[borrower].token = tokenvalue;
-        detail[borrower].loanamount = (((tokenvalue * 1 ether)/100 * 80) ) / 1 ether;
-        detail[borrower].duration = 12;
-        loan_count +=1;
-        //borrower.transfer(detail[borrower].loanamount);
-        //initial_ether -= detail[borrower].loanamount;
-       
-    }
-   
-    function transferther(address borrower) public payable 
-    {
-        borrower.transfer(msg.value);
-        initial_ether -= msg.value;
-    }
-    
-    function viewloanamount(address borrower)public view returns(uint256)
-    {
-        return(detail[borrower].loanamount);
-        
-    }
-    
->>>>>>> e33bb485124b96eebe86669e641f20f5e4e0e2ef
-   
-    }
